101b21881ea9b3b0cd9056b57f999e7732f925ea
Siarhei Prymakovich <prima.psa@gmail.com>
2023-02-18 12:24:52 +0300

    feat: implement component Profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import styles from "./Profile.module.css"
 import MyPosts from "./MyPosts/MyPosts";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
     return (
         <div className='content'>
-            <div className={styles.header}>
-                <img className={styles.img}
-                    src="https://gamerwall.pro/uploads/posts/2022-03/1648497221_1-gamerwall-pro-p-fioletovii-fon-dlya-shapki-krasivie-1.jpg"
-                    alt=""/>
-            </div>
-            <MyPosts />
+            <ProfileInfo/>
+            <MyPosts/>
         </div>
     );
 }
