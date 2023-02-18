49efd73f57db960533770cd0003d801bcfb6e65a
Siarhei Prymakovich <prima.psa@gmail.com>
2023-02-18 12:24:29 +0300

    feat: implement component ProfioleINfo

---
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./ProfileInfo.module.css"
+
+
+const ProfileInfo = () => {
+    return (
+        <>
+            <div className={styles.header}>
+                <img className={styles.img}
+                     src="https://gamerwall.pro/uploads/posts/2022-03/1648497221_1-gamerwall-pro-p-fioletovii-fon-dlya-shapki-krasivie-1.jpg"
+                     alt=""/>
+            </div>
+        </>
+
+
+    );
+}
+export default ProfileInfo;
