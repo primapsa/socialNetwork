daf87df9a0ccf22362532a07da5539fb76bd591f
Siarhei Prymakovich <prima.psa@gmail.com>
2023-03-10 13:38:58 +0300

    feat: implement profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,12 +2,21 @@ import React from "react";
 import styles from "./Profile.module.css"
 import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
+import {ProfileType} from "../../types";
+import {ProfilePropsType} from "./types";
 
-const Profile = () => {
+const Profile = (props: ProfilePropsType) => {
     return (
         <div className='content'>
             <ProfileInfo/>
-            <MyPosts/>
+            <MyPosts
+                state = {props.state}
+                dispatch = {props.dispatch}
+                // posts={props.userPosts}
+                // addPost={props.addPost}
+                // inputValue={props.inputValue}
+                // addNewInputValue={props.addNewInputValue}
+            />
         </div>
     );
 }
