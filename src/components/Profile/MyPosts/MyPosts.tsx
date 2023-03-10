75d974242e939cc240219d78ad4e625f4b77f544
Siarhei Prymakovich <prima.psa@gmail.com>
2023-03-10 13:39:15 +0300

    feat: implement myposts

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, {ChangeEvent, LegacyRef, useRef} from "react";
 import styles from "./MyPosts.module.css"
 import Post from "../Post/Post";
+import {MyPostsPropsType, UserPosts} from "../types";
+import {log} from "util";
 
-const MyPosts = () => {
+const MyPosts = ({dispatch, state}:MyPostsPropsType) => {
+    const inputValue: LegacyRef<HTMLTextAreaElement> = React.createRef();
+    const {posts} = state;
+    const calue = inputValue?.current?.value || '';
+    const addPost = () => {
+
+      //  props.addPost(calue);
+        dispatch({type: 'ADD_NEW_POST'})
+    }
+    const onChangeInputHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        let caluee = inputValue?.current?.value || '';
+       dispatch({type: 'ADD_NEW_INPUT', inputValue: caluee })
+       // props.addNewInputValue(caluee);
+    }
+
     return (
         <>
             <div>User Avatar + description</div>
             <div>
-                <textarea></textarea>
-                <button>Add post</button>
+                <textarea ref={inputValue} value={posts.inputValue} onChange={onChangeInputHandler}></textarea>
+                <button onClick={addPost}>Add post</button>
             </div>
-            <Post message ="Hello, how are you?"/>
-            <Post message = "It is my first post!"/>
-            <Post/>
+            {
+               posts.postsText.map(p => <Post key={p.id} id={p.id} message={p.message}/>)
+            }
+
         </>
     );
 }
