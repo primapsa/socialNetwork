a87ce8ae730e4e4a4a56d41e70c33db25752aa38
Siarhei Prymakovich <prima.psa@gmail.com>
2023-03-26 11:45:28 +0300

    feat: add route

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,41 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route, Router} from "react-router-dom";
+import {AppType, StateType} from "./types";
 
-function App() {
+
+
+function App({state, dispatch}: AppType) {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Navbar/>
-            <Profile/>
-        </div>
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div>
+                    <Route path='/profile'
+                           render={() => <Profile
+                               dispatch = {dispatch}
+                               state = {state}
+                               // userPosts={props.state.posts.postsText}
+                               // dispatch = {props.dispatch}
+                               // state = {props.state}
+                               // addPost={props.addPost}
+                               // inputValue={props.state.posts.inputValue}
+                               // addNewInputValue={props.addNewInputValue}
+                           />}/>
+                    <Route path='/messages'
+                           render={() =>
+                               <Dialogs
+                                   state={state}
+                                   dispatch={dispatch}
+                               />}/>
+                </div>
+
+            </div>
+        </BrowserRouter>
+
     );
 }
 
