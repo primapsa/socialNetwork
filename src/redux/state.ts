dd47fed0f119894edd9394cb23ed5aba5e29c020
Siarhei Prymakovich <prima.psa@gmail.com>
2023-03-26 11:48:38 +0300

    feat: add state

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,6 +1,6 @@
-import {stateInType, StoreType} from "../types";
-
-
+import {StoreType} from "../types";
+import postsReducer from "./postsReducer";
+import messagesReducer from "./messagesReducer";
 
 
 const stateValue = {
@@ -46,42 +46,57 @@ export let store: StoreType = {
     getState() {
         return this._state;
     },
-    _subscriber(observer){ console.log('Epmty')},
-    // addNewPost(){
-    //
-    //     const posts = this.state.posts.postsText;
-    //     const lastPostId = posts.length;
-    //    posts.push({id: lastPostId, message: this.state.posts.inputValue})
-    //
-    //    this.state.posts.inputValue = '';
-    //     this._subscriber(this.state);
-    // },
-    // addNewInputValue(inputValue: string){
-    //
-    //     this.state.posts.inputValue = inputValue;
-    //     this._subscriber(this.state);
-    // },
+    _subscriber(observer) {
+
+    },
+
     subscribe(observer) {
         this._subscriber = observer;
     },
-    dispatch (action){
-        if(action.type === 'ADD_NEW_INPUT'){
-            this._state.posts.inputValue = action.inputValue;
-            this._subscriber(this._state);
-        }
-        if(action.type === 'ADD_NEW_POST'){
-            const posts = this._state.posts.postsText;
-            const lastPostId = posts.length;
-            posts.push({id: lastPostId, message: this._state.posts.inputValue})
+    dispatch(action) {
 
-            this._state.posts.inputValue = '';
-            this._subscriber(this._state);
-        }
-        if(action.type === 'ADD-NEW-MESSAGE-TEXT'){
 
-        }
+        this._state.posts = postsReducer(this._state.posts, action)
+        this._state.messages = messagesReducer(this._state.messages, action)
+        this._subscriber(this._state);
+
+        //
+        // if (action.type === 'ADD_NEW_INPUT') {
+        //     this._state.posts.inputValue = action.inputValue;
+        //     this._subscriber(this._state);
+        // }
+        // if (action.type === 'ADD_NEW_POST') {
+        //     const posts = this._state.posts.postsText;
+        //     const lastPostId = posts.length;
+        //     posts.push({id: lastPostId, message: this._state.posts.inputValue})
+        //
+        //     this._state.posts.inputValue = '';
+        //     this._subscriber(this._state);
+        // }
+        // if (action.type === 'ADD-NEW-MESSAGE-TEXT') {
+        //     this._state.messages.messagesTextarea = action.value;
+        //     this._subscriber(this._state);
+        // }
+        // if (action.type === 'ADD-NEW-USER-MESSAGE') {
+        //     debugger
+        //     const pushedNewElement = [...this._state.messages.messagesText, {
+        //         id: 9,
+        //         message: this._state.messages.messagesTextarea
+        //     }]
+        //
+        //     const temp = {
+        //         ...this._state,
+        //         messages: {...this._state.messages, messagesText: pushedNewElement, messagesTextarea: ''}
+        //     }
+        //     this._state = temp;
+        //     this._subscriber(this._state);
+        // }
+
     }
 }
 
-
+export const addNewMessageTextAC = (value: string) => ({type: 'ADD-NEW-MESSAGE-TEXT', value: value})
+export const addNewUserMessageAC = () => ({type: 'ADD-NEW-USER-MESSAGE'})
+export const addNewPost = () => ({type: 'ADD_NEW_POST'})
+export const addNewPostText = (value: string) => ({type: 'ADD_NEW_INPUT', inputValue: value})
 export default store;
