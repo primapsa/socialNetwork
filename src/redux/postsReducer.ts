e50f960b663b75200cad986caec9bf9bcc03f2a3
Siarhei Prymakovich <prima.psa@gmail.com>
2023-03-27 14:36:56 +0300

    feat: add posts initial

--- src/redux/postsReducer.ts
+++ src/redux/postsReducer.ts
@@ -2,8 +2,20 @@ import {ActionTypeExtended, PostsInnerStateType} from "../types";
 
 export const addNewPost = () => ({type: 'ADD_NEW_POST'})
 export const addNewPostText = (value: string) => ({type: 'ADD_NEW_INPUT', inputValue: value})
-
-const PostsReducer = (state : PostsInnerStateType, action: ActionTypeExtended): PostsInnerStateType => {
+const initial = {
+    postsText: [
+        {id: 1, message: 'Hello, how are you&'},
+        {id: 2, message: 'What is going on?'},
+        {id: 3, message: 'Nothing'},
+        {id: 4, message: 'Good'},
+        {id: 5, message: 'What is wrong?'},
+        {id: 6, message: 'Dont worry'},
+        {id: 7, message: 'Be happy'},
+        {id: 8, message: 'Okay'},
+    ],
+    inputValue: ''
+}
+const PostsReducer = (state : PostsInnerStateType = initial, action: ActionTypeExtended): PostsInnerStateType => {
     switch (action.type) {
         case 'ADD_NEW_INPUT':
             state.inputValue = action.inputValue as string
