292903a05b4315f95a958ba24cd23831ad45f666
Siarhei Prymakovich <prima.psa@gmail.com>
2023-03-27 14:55:38 +0300

    refactor: rm unused imports

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './index.css';
 import store from "./redux/redux-store";
 import App from "./App";
-import {AppType, stateInType, StoreType} from "./types";
 import ReactDOM from 'react-dom';
 import {Provider} from "react-redux";
 
