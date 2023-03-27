7ecea3df4682fd827ff825f1b7d648999c0225a3
Siarhei Prymakovich <prima.psa@gmail.com>
2023-03-27 15:17:46 +0300

    refactor: change state immutable

--- src/redux/messagesReducer.ts
+++ src/redux/messagesReducer.ts
@@ -1,34 +1,38 @@
-import {ActionTypeExtended, MessagesStateType} from "../types";
+import {MessagesStateType} from "../types";
 
-export const addNewMessageTextAC = (value: string) => ({type: 'ADD-NEW-MESSAGE-TEXT', value: value})
-export const addNewUserMessageAC = () => ({type: 'ADD-NEW-USER-MESSAGE'})
+type ActionType = AddNewUserMessageACType | AddNewMessageTextACype
+type AddNewUserMessageACType = ReturnType<typeof addNewUserMessageAC>
+type AddNewMessageTextACype = ReturnType<typeof addNewMessageTextAC>
+
+export const addNewMessageTextAC = (value: string) => ({type: 'ADD-NEW-MESSAGE-TEXT', value: value} as const)
+export const addNewUserMessageAC = () => ({type: 'ADD-NEW-USER-MESSAGE'} as const)
+
 const initial = {
-        messagesText: [
-            {id: 1, message: 'Hello, how are you&'},
-            {id: 2, message: 'What is going on?'},
-            {id: 3, message: 'Nothing'},
-            {id: 4, message: 'Good'},
-            {id: 5, message: 'What is wrong?'},
-            {id: 6, message: 'Dont worry'},
-            {id: 7, message: 'Be happy'},
-            {id: 8, message: 'Okay'},
-        ],
-        messagesTextarea: ''
-    }
-const messagesReducer = (state:MessagesStateType = initial , action: ActionTypeExtended): MessagesStateType => {
+    messagesText: [
+        {id: 1, message: 'Hello, how are you&'},
+        {id: 2, message: 'What is going on?'},
+        {id: 3, message: 'Nothing'},
+        {id: 4, message: 'Good'},
+        {id: 5, message: 'What is wrong?'},
+        {id: 6, message: 'Dont worry'},
+        {id: 7, message: 'Be happy'},
+        {id: 8, message: 'Okay'},
+    ],
+    messagesTextarea: ''
+}
+
+const messagesReducer = (state: MessagesStateType = initial, action: ActionType): MessagesStateType => {
 
     switch (action.type) {
         case 'ADD-NEW-MESSAGE-TEXT' :
-            state.messagesTextarea = action.value as string
-            return state;
+
+            return {...state, messagesTextarea: action.value}
+
         case 'ADD-NEW-USER-MESSAGE' :
-            const pushedNewElement = [...state.messagesText, {
-                id: 9,
-                message: state.messagesTextarea
-            }]
-            state.messagesText = pushedNewElement;
-            state.messagesTextarea = '';
-            return state;
+
+            const newMessage = {id: state.messagesText.length + 1, message: state.messagesTextarea}
+            return {...state, messagesText: [...state.messagesText, newMessage], messagesTextarea: ''}
+
         default :
             return state;
 
