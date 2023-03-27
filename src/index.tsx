e71e3e5465f05a989ce0355705ada05a7eb6e9d4
Siarhei Prymakovich <prima.psa@gmail.com>
2023-03-27 14:35:49 +0300

    feat: wrap app with redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import './index.css';
-import store from "./redux/state";
+import store from "./redux/redux-store";
 import App from "./App";
 import {AppType, stateInType, StoreType} from "./types";
 import ReactDOM from 'react-dom';
+import {Provider} from "react-redux";
 
 
 const rerenderTree = (state: any) => {
     ReactDOM.render(
-        <App
+       <Provider store={store}>
+           <App
+               // state={store.getState()}
+               // dispatch = {store.dispatch.bind(store)}
 
-            state={store.getState()}
-            dispatch = {store.dispatch.bind(store)}
-            // addPost={store.addNewPost.bind(store)}
-            // inputValue={store.state.posts.inputValue}
-            // addNewInputValue={store.addNewInputValue.bind(store)}
-        />,
+           />
+       </Provider>
+        ,
         document.getElementById('root')
     );
 }
 rerenderTree(store.getState());
-store.subscribe(rerenderTree);
+store.subscribe(() => rerenderTree(store.getState()));
 
 export default rerenderTree;
 
