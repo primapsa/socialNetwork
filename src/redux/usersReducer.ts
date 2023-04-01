b007de4d7732799f4935f41756697257c4d0b986
Siarhei Prymakovich <prima.psa@gmail.com>
2023-04-01 14:34:55 +0300

    feat: add user reducer

--- src/redux/usersReducer.ts
+++ src/redux/usersReducer.ts
@@ -1,17 +1,62 @@
-import {ActionTypeExtended, UsersStateType} from "../types";
+
+export const followAC = (id: number) => ({type: 'FOLLOW', payload: {id}} as const)
+export const unfollowAC = (id: number) => ({type: 'UNFOLLOW', payload: {id}} as const)
+
+type ActionType = FollowACType | UnFollowACType
+type FollowACType = ReturnType<typeof followAC>
+type UnFollowACType = ReturnType<typeof unfollowAC>
 
 const initial = [
-    {id: 1, username: 'Vasya'},
-    {id: 2, username: 'Igor'},
-    {id: 3, username: 'Lena'},
-    {id: 4, username: 'Mashs'},
-    {id: 5, username: 'Sveta'},
-    {id: 6, username: 'VAlera'},
-    {id: 7, username: 'Sergey'},
-    {id: 8, username: 'VAdim'},
+    {
+        id: 1,
+        data: {firstName: 'Kolya', lastName: 'Ivanov', photo: 'https://us.123rf.com/450wm/tuktukdesign/tuktukdesign1606/tuktukdesign160600107/59070188-user-icon-mann-profil-gesch%C3%A4ftsmann-avatar-person-symbol-in-vektor-illustration.jpg'},
+        place: {country: 'Belarus', city: 'Minsk'},
+        status: 'Here i am',
+        isFollow: true
+    },{
+        id: 2,
+        data: {firstName: 'Kolyaaaa', lastName: 'Ivanaaaaov', photo: 'https://us.123rf.com/450wm/tuktukdesign/tuktukdesign1606/tuktukdesign160600107/59070188-user-icon-mann-profil-gesch%C3%A4ftsmann-avatar-person-symbol-in-vektor-illustration.jpg'},
+        place: {country: 'Belarus', city: 'Minsk'},
+        status: 'Here i am',
+        isFollow: false
+    },{
+        id: 3,
+        data: {firstName: 'Kodfdlya', lastName: 'Ivadsfdnov', photo: 'https://us.123rf.com/450wm/tuktukdesign/tuktukdesign1606/tuktukdesign160600107/59070188-user-icon-mann-profil-gesch%C3%A4ftsmann-avatar-person-symbol-in-vektor-illustration.jpg'},
+        place: {country: 'Belarus', city: 'Minsk'},
+        status: 'Here i am',
+        isFollow: true
+    }
 ]
-const usersReducer = (state : UsersStateType[] = initial, action: ActionTypeExtended): UsersStateType[] => {
-  return state
+export type UsersStateType = {
+    id: number
+    data: userType
+    place: placeType
+    status: string
+    isFollow: boolean
+}
+type userType = {
+    firstName: string
+    lastName: string
+    photo: string
+}
+type placeType = {
+    country: string;
+    city: string
+}
+const UsersReducer = (state: UsersStateType[] = initial, action: ActionType): UsersStateType[] => {
+
+    switch (action.type) {
+
+        case 'FOLLOW':
+         return state.map(user => user.id == action.payload.id ? {...user, isFollow: true} : user)
+
+        case 'UNFOLLOW':
+            return state.map(user => user.id == action.payload.id ? {...user, isFollow: false} : user)
+
+        default :
+            return state;
+    }
 
 }
-export default usersReducer
+
+export default UsersReducer
