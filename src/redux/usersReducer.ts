19d85cb8f6a76bee24cf417e871fb4e98c4c0971
Siarhei Prymakovich <prima.psa@gmail.com>
2023-04-12 11:56:17 +0300

    feat: user-reducer cases

--- src/redux/usersReducer.ts
+++ src/redux/usersReducer.ts
@@ -1,39 +1,44 @@
 
 export const followAC = (id: number) => ({type: 'FOLLOW', payload: {id}} as const)
 export const unfollowAC = (id: number) => ({type: 'UNFOLLOW', payload: {id}} as const)
+export const setUsersAC = (users: UsersType[]) => ({type: 'SET-USERS', payload: {users}} as const)
+export const setCurrentPageAC = (page: number) => ({type: 'SET-CURRENT-PAGE', payload: {page}} as const)
 
-type ActionType = FollowACType | UnFollowACType
+
+type ActionType = FollowACType | UnFollowACType | SetUsersACType | setCurrentPageACType
 type FollowACType = ReturnType<typeof followAC>
 type UnFollowACType = ReturnType<typeof unfollowAC>
+type SetUsersACType = ReturnType<typeof setUsersAC>
+type setCurrentPageACType = ReturnType<typeof setCurrentPageAC>
 
-const initial = [
-    {
-        id: 1,
-        data: {firstName: 'Kolya', lastName: 'Ivanov', photo: 'https://us.123rf.com/450wm/tuktukdesign/tuktukdesign1606/tuktukdesign160600107/59070188-user-icon-mann-profil-gesch%C3%A4ftsmann-avatar-person-symbol-in-vektor-illustration.jpg'},
-        place: {country: 'Belarus', city: 'Minsk'},
-        status: 'Here i am',
-        isFollow: true
-    },{
-        id: 2,
-        data: {firstName: 'Kolyaaaa', lastName: 'Ivanaaaaov', photo: 'https://us.123rf.com/450wm/tuktukdesign/tuktukdesign1606/tuktukdesign160600107/59070188-user-icon-mann-profil-gesch%C3%A4ftsmann-avatar-person-symbol-in-vektor-illustration.jpg'},
-        place: {country: 'Belarus', city: 'Minsk'},
-        status: 'Here i am',
-        isFollow: false
-    },{
-        id: 3,
-        data: {firstName: 'Kodfdlya', lastName: 'Ivadsfdnov', photo: 'https://us.123rf.com/450wm/tuktukdesign/tuktukdesign1606/tuktukdesign160600107/59070188-user-icon-mann-profil-gesch%C3%A4ftsmann-avatar-person-symbol-in-vektor-illustration.jpg'},
-        place: {country: 'Belarus', city: 'Minsk'},
-        status: 'Here i am',
-        isFollow: true
-    }
-]
-export type UsersStateType = {
-    id: number
-    data: userType
-    place: placeType
-    status: string
-    isFollow: boolean
+const initial = {
+    userPage: [],
+    userPerPage: 10,
+    totalCount: 50,
+    currentPage: 1
 }
+export type UsersStateType = {
+    userPage: UsersType[]
+    userPerPage: number
+    totalCount: number
+    currentPage: number
+}
+export type UsersType = {
+    id: number
+    name: string
+    followed: boolean
+    photos: PhotoType
+    status: string | null
+    uniqueUrlName: string | null
+    data?: userType
+    place?: placeType
+    isFollow?: boolean
+}
+type PhotoType = {
+    small: string | null
+    large: string | null
+}
+
 type userType = {
     firstName: string
     lastName: string
@@ -43,15 +48,21 @@ type placeType = {
     country: string;
     city: string
 }
-const UsersReducer = (state: UsersStateType[] = initial, action: ActionType): UsersStateType[] => {
+const UsersReducer = (state: UsersStateType = initial, action: ActionType): UsersStateType => {
 
     switch (action.type) {
 
         case 'FOLLOW':
-         return state.map(user => user.id == action.payload.id ? {...user, isFollow: true} : user)
+         return {...state, userPage: state.userPage.map(user => user.id == action.payload.id ? {...user, followed: true} : user)}
 
         case 'UNFOLLOW':
-            return state.map(user => user.id == action.payload.id ? {...user, isFollow: false} : user)
+            return {...state, userPage: state.userPage.map(user => user.id == action.payload.id ? {...user, followed: false} : user)}
+
+        case 'SET-USERS':
+            return {...state, userPage: action.payload.users}
+
+        case 'SET-CURRENT-PAGE':
+            return {...state, currentPage: action.payload.page}
 
         default :
             return state;
