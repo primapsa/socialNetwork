a779ed827fb482e2658f208890c4ae2283ca0d75
Siarhei Prymakovich <prima.psa@gmail.com>
2023-04-12 11:57:04 +0300

    feat: user-cobtaoner props

--- src/components/Users/UsersContainer.ts
+++ src/components/Users/UsersContainer.ts
@@ -2,13 +2,22 @@ import store, {StateType} from "../../redux/redux-store";
 import {connect} from "react-redux";
 import Users from "./Users";
 import {Dispatch} from "redux";
-import {followAC, unfollowAC} from "../../redux/usersReducer";
+import {followAC, setCurrentPageAC, setUsersAC, unfollowAC, UsersType} from "../../redux/usersReducer";
 
-const mapStateToProp = (state: StateType) => ({users: state.users})
+const mapStateToProp = (state: StateType) => (
+    {
+        users: state.users.userPage,
+        userPerPage: state.users.userPerPage,
+        totalCount: state.users.totalCount,
+        currentPage: state.users.currentPage
+    }
+)
 const mapDispatchToProps = (dispatch: Dispatch) => (
     {
         follow: (id: number) => dispatch(followAC(id)),
         unfollow: (id: number) => dispatch(unfollowAC(id)),
+        setUsers: (users: UsersType[]) => dispatch(setUsersAC(users)),
+        setCurrentPage: (page: number) => dispatch(setCurrentPageAC(page))
     })
 
 
