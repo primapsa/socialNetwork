8dc2b69a0446515508eb3d1198ab1d2f80be3979
Siarhei Prymakovich <prima.psa@gmail.com>
2023-04-14 18:35:31 +0300

    refactor: app

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,8 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
-import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route, Router} from "react-router-dom";
-import {StateType} from "./types";
+import {BrowserRouter, Route} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import Users from "./components/Users/Users";
 import UsersContainer from "./components/Users/UsersContainer";
 
 type AppPropsType = {};
