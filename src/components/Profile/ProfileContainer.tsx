43177651486c3395ff03177a5713ad813f3275c3
Siarhei Prymakovich <prima.psa@gmail.com>
2023-04-18 19:43:47 +0300

    feat: add withrouter

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -5,16 +5,24 @@ import {StateType} from "../../redux/redux-store";
 import {addNewPost, addNewPostText, setProfile} from "../../redux/postsReducer";
 import {PostsInnerStateType, ProfileInnerType} from "../../types";
 import axios from "axios";
-import {log} from "util";
-type ProfileContainerPropsTytpe = {
+
+import {RouteComponentProps, withRouter} from "react-router-dom";
+
+type MatchParamType = {
+    userId: string
+}
+
+type ProfileContainerPropsTytpe = RouteComponentProps<MatchParamType> & {
     profilePosts: PostsInnerStateType;
     addNewPost: () => void
     addNewPostText: (text: string) => void
     setProfile: (profile: ProfileInnerType | null) => void
+
 }
+
 class ProfileContainer extends React.Component<ProfileContainerPropsTytpe> {
     componentDidMount() {
-        const userID = 2
+        const userID = this.props.match.params.userId
         axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userID}`).then( response => this.props.setProfile(response.data))
     }
 
@@ -25,5 +33,5 @@ class ProfileContainer extends React.Component<ProfileContainerPropsTytpe> {
 const mapStateToProps = (state: StateType) => ({
  profilePosts: state.posts
 })
-
-export default connect(mapStateToProps, {addNewPost, addNewPostText, setProfile})(ProfileContainer);
+const ProfileContainerWithRouter = withRouter(ProfileContainer)
+export default connect(mapStateToProps, {addNewPost, addNewPostText, setProfile})(ProfileContainerWithRouter);
