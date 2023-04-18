4a9666b6589c9da5ef17c8d9b4ade436f8fa4d37
Siarhei Prymakovich <prima.psa@gmail.com>
2023-04-18 19:41:34 +0300

    refactor: route to profile

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Profile from "./components/Profile/Profile";
 import {BrowserRouter, Route} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
+import ProfileContainer from "./components/Profile/ProfileContainer";
 
 type AppPropsType = {};
 
@@ -16,7 +17,7 @@ function App (props: AppPropsType) {
                 <Header/>
                 <Navbar/>
                 <div>
-                    <Route path='/profile' component={Profile} />
+                    <Route path='/profile/:userId?' component={ProfileContainer} />
                     <Route path='/messages' component={DialogsContainer} />
                     <Route path='/users' component={UsersContainer} />
                 </div>
