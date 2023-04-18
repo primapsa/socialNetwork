82c99c036fc7c95c8ca025353fe9708a4c1e862d
Siarhei Prymakovich <prima.psa@gmail.com>
2023-04-18 19:44:06 +0300

    feat: add image and title

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import styles from "./ProfileInfo.module.css"
+import {PostsInnerStateType} from "../../../types";
 
+type ProfileInfoPropsType = {
+    userInfo: PostsInnerStateType
+}
 
-const ProfileInfo = () => {
+const ProfileInfo = ({userInfo}:ProfileInfoPropsType) => {
     return (
         <>
             <div className={styles.header}>
                 <img className={styles.img}
                      src="https://gamerwall.pro/uploads/posts/2022-03/1648497221_1-gamerwall-pro-p-fioletovii-fon-dlya-shapki-krasivie-1.jpg"
                      alt=""/>
+            </div>
+            <div>
+                <img src={userInfo?.profile?.photos.large} alt=""/>
+                <h4>{userInfo.profile?.fullName}</h4>
             </div>
         </>
 
