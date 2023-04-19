92c1a2a321883dc31b498c632561809593bd3b3d
Siarhei Prymakovich <prima.psa@gmail.com>
2023-04-19 12:49:42 +0300

    feat: add auth to store

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -2,6 +2,7 @@ import {combineReducers, createStore} from "redux";
 import messagesReducer from "./messagesReducer";
 import PostsReducer from "./postsReducer";
 import usersReducer from "./usersReducer";
+import authReducer from "./authReducer";
 
 
 export type StateType = ReturnType<typeof rootReducer>
@@ -9,7 +10,8 @@ export type StateType = ReturnType<typeof rootReducer>
 const rootReducer = combineReducers({
     messages: messagesReducer,
     posts: PostsReducer,
-    users: usersReducer
+    users: usersReducer,
+    auth: authReducer
 })
 
 export const store = createStore(rootReducer)
