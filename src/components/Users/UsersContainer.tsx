d5d155f664788c5047bde06f533e4ce560073665
Siarhei Prymakovich <prima.psa@gmail.com>
2023-04-19 15:35:42 +0300

    feat: button disabling atributes

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -2,7 +2,15 @@ import React from "react";
 import store, {StateType} from "../../redux/redux-store";
 import {connect} from "react-redux";
 import {Dispatch} from "redux";
-import {follow, setCurrentPage, setTotalCount, setUsers, unfollow, UsersType} from "../../redux/usersReducer";
+import {
+    follow,
+    setCurrentPage,
+    setTotalCount,
+    setUsers,
+    toggleMakingRequest,
+    unfollow,
+    UsersType
+} from "../../redux/usersReducer";
 import axios from "axios";
 import Users from "./Users";
 
@@ -17,13 +25,15 @@ type UsersConatinerOuterPropsType = {
     setUsers: (users: UsersType[]) => void
     setCurrentPage: (page: number) => void
     setTotalCount: (totalCount: number) => void
+    toggleMakingRequest: (isFetching: boolean, userID: number) => void
+    makingRequestFor: number[]
 }
 
 
 class UsersContainer extends React.Component<UsersConatinerOuterPropsType> {
 
     componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.userPerPage}`).then(response => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.userPerPage}`, {withCredentials: true}).then(response => {
             this.props.setTotalCount(response.data.totalCount)
             this.props.setUsers(response.data.items)
         })
@@ -31,7 +41,7 @@ class UsersContainer extends React.Component<UsersConatinerOuterPropsType> {
     onHandler = (page: number) => {
 
         this.props.setCurrentPage(page)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.userPerPage}`).then(response => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.userPerPage}`, {withCredentials: true}).then(response => {
             this.props.setUsers(response.data.items)
         })
     }
@@ -48,6 +58,8 @@ class UsersContainer extends React.Component<UsersConatinerOuterPropsType> {
                         unfollow={this.props.unfollow}
                         currentPage={this.props.currentPage}
                         setPage={this.onHandler}
+                        toggleMakingRequest = {this.props.toggleMakingRequest}
+                        makingRequestFor = {this.props.makingRequestFor}
                 />
             </>
         );
@@ -60,7 +72,8 @@ const mapStateToProp = (state: StateType) => (
         users: state.users.userPage,
         userPerPage: state.users.userPerPage,
         totalCount: state.users.totalCount,
-        currentPage: state.users.currentPage
+        currentPage: state.users.currentPage,
+        makingRequestFor: state.users.makingRequestFor
     }
 )
 // const mapDispatchToProps = (dispatch: Dispatch) => (
@@ -74,4 +87,4 @@ const mapStateToProp = (state: StateType) => (
 
 
 
-export default connect(mapStateToProp, {follow, unfollow, setUsers, setCurrentPage, setTotalCount})(UsersContainer)
+export default connect(mapStateToProp, {follow, unfollow, setUsers, setCurrentPage, setTotalCount, toggleMakingRequest})(UsersContainer)
