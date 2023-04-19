14648d3727f09d06751dd543ccde48bd7d2f4438
Siarhei Prymakovich <prima.psa@gmail.com>
2023-04-19 15:34:51 +0300

    refactor: user api object axios

--- src/components/Users/User/User.tsx
+++ src/components/Users/User/User.tsx
@@ -3,17 +3,36 @@ import styles from './User.module.css'
 import {UsersType} from "../../../redux/usersReducer";
 import avatar from './../../../image/avatar.png'
 import {NavLink} from "react-router-dom";
+import axios from "axios";
+import {userAPI} from "../../../api/api";
 
 type UserPropsType = {
     user: UsersType
     follow: (id: number) => void
     unfollow: (id: number) => void
+    toggleMakingRequest: (isFetching: boolean, userID: number) => void
+    makingRequestFor: number[]
 }
 
-const User = ({user, follow, unfollow}: UserPropsType) => {
+const User = ({user, follow, unfollow, toggleMakingRequest, makingRequestFor}: UserPropsType) => {
+
     const onclickHandler = () => {
 
-        user.followed ? unfollow(user.id) : follow(user.id)
+        toggleMakingRequest(true, user.id)
+
+        user.followed ? userAPI.unfollowUser(user.id)
+                .then(response => {
+                    if (response.resultCode === 0) {
+                        unfollow(user.id)
+                    }
+                    toggleMakingRequest(false,user.id)
+                }) :
+            userAPI.followUser(user.id).then(response => {
+                if (response.resultCode === 0) {
+                    follow(user.id)
+                }
+                toggleMakingRequest(false,user.id)
+            });
     }
 
     return (
@@ -22,7 +41,7 @@ const User = ({user, follow, unfollow}: UserPropsType) => {
                 <NavLink to={`profile/${user.id}`}>
                     <img className={styles.avatar} src={user.photos.small ? user.photos.small : avatar} alt="logo"/>
                 </NavLink>
-                <button onClick={onclickHandler}>{user.followed ? 'FOLLOW' : 'UNFOLLOW'}</button>
+                <button onClick={onclickHandler} disabled={makingRequestFor.some(e => e === user.id)}>{!user.followed ? 'FOLLOW' : 'UNFOLLOW'}</button>
             </div>
             <div className={styles.row}>
                 <span>{user.name}</span>
