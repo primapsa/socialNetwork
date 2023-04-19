a19769175818bf3d783b89279d8252d642b52103
Siarhei Prymakovich <prima.psa@gmail.com>
2023-04-19 12:49:22 +0300

    feat: add greeting to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import styles from "./Header.module.css"
+import {UserAuthStateType} from "../../redux/authReducer";
+import Greeting from "./Greeting/Greeting";
 
-const Header = () => {
+type HeaderPropsType = {
+    auth: UserAuthStateType
+}
+const Header = ({auth}: HeaderPropsType) => {
+
     return (
         <header className={styles.header}>
             <img src="https://upload.wikimedia.org/wikipedia/commons/8/84/Mozilla_Firefox_3.5_logo.png?20130709084931"
                  alt=""/>
+            <Greeting auth={auth}/>
         </header>
     );
 }
