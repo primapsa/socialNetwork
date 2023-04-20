1b11e63c6fe35badae7a6ef9079148481e5f9384
Siarhei Prymakovich <prima.psa@gmail.com>
2023-04-20 20:53:20 +0300

    refactor: side effect query from api

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,15 +4,19 @@ const axiosInstance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     withCredentials: true
 })
-type deleteUserPropmiseType = {
+type deleteUserPromiseType = {
     resultCode: number
 }
 export const userAPI = {
-    unfollowUser: (userID: number): Promise<deleteUserPropmiseType> => axiosInstance.delete(`follow/${userID}`)
+    unfollowUser: (userID: number): Promise<deleteUserPromiseType> => axiosInstance.delete(`follow/${userID}`)
         .then(response => response.data),
 
-    followUser:(userID: number): Promise<any> => axiosInstance.post(`follow/${userID}`,{})
-        .then( response => response.data)
+    followUser: (userID: number): Promise<any> => axiosInstance.post(`follow/${userID}`, {})
+        .then(response => response.data)
 
 
+}
+
+export const authAPI = {
+    me: () => axiosInstance.get(`auth/me`).then( response => response.data)
 }
--- src/components/Header/HeaderContainer.tsx
+++ src/components/Header/HeaderContainer.tsx
@@ -4,18 +4,14 @@ import {connect} from "react-redux";
 import {StateType} from "../../redux/redux-store";
 import {setUserAuth, UserAuthStateType} from "../../redux/authReducer";
 import axios from "axios";
+import {authAPI} from "../../api/api";
 type HeaderContainerPropsType = {
     auth: UserAuthStateType
     setUserAuth: (data: UserAuthStateType) => void
 }
 class HeaderContainer extends React.Component<HeaderContainerPropsType> {
     componentDidMount() {
-        axios.get('https://social-network.samuraijs.com/api/1.0/auth/me', {withCredentials: true}).then(
-            resolve => {
-                this.props.setUserAuth(resolve.data.data)
-            }
-
-        )
+        authAPI.me().then( resolve => this.props.setUserAuth(resolve.data.data))
     }
 
     render() {
