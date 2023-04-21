bbe5e200b25173f9a9dcc31f75d367009c0a2425
Siarhei Prymakovich <prima.psa@gmail.com>
2023-04-21 17:54:35 +0300

    refactor: use thunk

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -2,11 +2,10 @@ import React from "react";
 import Profile from "./Profile";
 import {connect} from "react-redux";
 import {StateType} from "../../redux/redux-store";
-import {addNewPost, addNewPostText, setProfile} from "../../redux/postsReducer";
-import {PostsInnerStateType, ProfileInnerType} from "../../types";
+import {addNewPost, addNewPostText, setProfileThunk} from "../../redux/postsReducer";
+import {PostsInnerStateType} from "../../types";
 
 import {RouteComponentProps, withRouter} from "react-router-dom";
-import {profileAPI} from "../../api/api";
 
 type MatchParamType = {
     userId: string
@@ -16,14 +15,14 @@ type ProfileContainerPropsTytpe = RouteComponentProps<MatchParamType> & {
     profilePosts: PostsInnerStateType;
     addNewPost: () => void
     addNewPostText: (text: string) => void
-    setProfile: (profile: ProfileInnerType | null) => void
+    setProfileThunk: (userID: number) => void
 
 }
 
 class ProfileContainer extends React.Component<ProfileContainerPropsTytpe> {
     componentDidMount() {
         const userID = Number(this.props.match.params.userId)
-        profileAPI.getProfile(userID).then(response => this.props.setProfile(response.data))
+        this.props.setProfileThunk(userID)
     }
 
     render() {
@@ -36,4 +35,4 @@ const mapStateToProps = (state: StateType) => ({
     profilePosts: state.posts
 })
 const ProfileContainerWithRouter = withRouter(ProfileContainer)
-export default connect(mapStateToProps, {addNewPost, addNewPostText, setProfile})(ProfileContainerWithRouter);
+export default connect(mapStateToProps, {addNewPost, addNewPostText, setProfileThunk})(ProfileContainerWithRouter);
