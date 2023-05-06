942016f5358380f3ea261f76d533f175c78c3fa9
Siarhei Prymakovich <prima.psa@gmail.com>
2023-05-06 15:35:50 +0300

    refactor:  replace type

--- src/redux/authReducer.ts
+++ src/redux/authReducer.ts
@@ -3,19 +3,7 @@ import {authAPI} from "../api/api";
 
 export const setUserAuth = (data: SetUserAuthAttributeType) => ({type: 'SET-USER-AUTH', payload: data} as const)
 export const authThunk = () => (dispatch: Dispatch) => {
-    authAPI.me().then( resolve => dispatch(setUserAuth(resolve.data)))
-}
-
-
-type SetUserAuthType = ReturnType<typeof setUserAuth>
-type SetUserAuthAttributeType = Omit<UserAuthStateType, 'isAuth'>
-
-
-export type UserAuthStateType = {
-    id: number | null
-    login: string | null
-    email: string | null
-    isAuth: boolean
+    authAPI.me().then(resolve => dispatch(setUserAuth(resolve.data)))
 }
 const initial = {
     id: null,
@@ -24,16 +12,25 @@ const initial = {
     isAuth: false
 
 }
-type ActionType = SetUserAuthType;
 const authReducer = (state: UserAuthStateType = initial, action: ActionType): UserAuthStateType => {
 
     switch (action.type) {
-        case 'SET-USER-AUTH':{
+        case 'SET-USER-AUTH': {
             return {...state, ...action.payload, isAuth: true}
         }
-
         default:
             return state
     }
 }
+
 export default authReducer
+
+type SetUserAuthType = ReturnType<typeof setUserAuth>
+type SetUserAuthAttributeType = Omit<UserAuthStateType, 'isAuth'>
+export type UserAuthStateType = {
+    id: number | null
+    login: string | null
+    email: string | null
+    isAuth: boolean
+}
+type ActionType = SetUserAuthType;
